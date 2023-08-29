c7ff647191388255e364f0e563ef22f6c1232376
abdullahinalcik <130457799+abdullahinalcik@users.noreply.github.com>
2023-08-29 12:33:59 +0300

    added: redirect nand persist

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import { ToastContainer } from "react-toastify";
 
+
 import "./App.css";
 
 
 import AppRouter from "./router/AppRouter";
 //*redux reduxtoolkit
-import { store } from "./app/store";
+import { persistor, store } from "./app/store";
 import { Provider } from "react-redux";
+//*refresh de login bilgilerini sesisonstorage atmak için
+import { PersistGate } from 'redux-persist/integration/react'
 
 function App() {
   return (
     <div>
       <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
         <AppRouter />
         <ToastContainer />
+        </PersistGate>
       </Provider>
     </div>
   );
--- src/app/store.jsx
+++ src/app/store.jsx
@@ -2,9 +2,24 @@ import { configureStore } from '@reduxjs/toolkit'
 import authReducer from "../features/authSlice"
 import blogReducer from "../features/blogSlice"
 
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage/session'
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, authReducer)
+
+
 export const store = configureStore({
   reducer: {
-    auth: authReducer,
+    auth: persistedReducer,
     blog: blogReducer,
   },
 })
+
+export const persistor = persistStore(store)
+
+export default store
